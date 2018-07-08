a924b8d8562fb21f002e79984c87c37c82322396
Tuduc666 <37839044+Tuduc666@users.noreply.github.com>
2018-07-08 18:04:30 -0400

    Add files via upload

---
+++ src/dao/GetConnection.java
@@ -0,0 +1,59 @@
+package dao;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class GetConnection {
+// Heroku connection
+//	public static Connection getConnection() throws ClassNotFoundException, IOException, SQLException {
+//		String dbUrl = System.getenv("JDBC_DATABASE_URL");
+//		return DriverManager.getConnection(dbUrl);
+//	}
+	
+// Postgres JDBC local connection - worked!!!!!
+//    private final static String url = "jdbc:postgresql://localhost/dvdrental";
+//    private final static String user = "postgres";
+//    private final static String password = "Tamduc1166";
+//    
+//    public static Connection Connect() {
+//        Connection conn = null;
+//        try {
+        	// this next line is needed for TomCat to work!!!!!!!
+//        	Class.forName("org.postgresql.Driver");            
+//            conn = DriverManager.getConnection(url, user, password);
+//            System.out.println("Connected to the PostgreSQL server successfully.");
+//        } catch (SQLException | ClassNotFoundException e) {
+//            System.out.println(e.getMessage());
+//        }
+// 
+//        return conn;
+//    }
+    
+ // Postgres JDBC local connection - worked!!!!!
+//    private final static String url = "jdbc:postgresql://localhost/dvdrental";
+//    private final static String user = "postgres";
+//    private final static String password = "Tamduc1166";
+    
+    public static Connection Connect() {
+        Connection conn = null;
+        try {
+        	URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        	String user = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String url = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+            
+        	// this next line is needed for TomCat to work!!!!!!!
+        	Class.forName("org.postgresql.Driver");       
+        	
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException | ClassNotFoundException | URISyntaxException e) {
+            System.out.println(e.getMessage());
+        }
+ 
+        return conn;
+    }
+}
